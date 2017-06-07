07d3da87f42814302ace8e8fed6325de057e099a
Lucas Slot <lfh.slot@gmail.com>
2017-06-07 23:48:26 +0200

    Replaced J.count() > 0 check and start at i=1 instead - needs testing

--- P1/src/solver.cpp
+++ P1/src/solver.cpp
@@ -114,16 +114,18 @@ Result Solver::Merge(Label *I_label) {
      * over all valid (v, J) instead (regardless of whether 
      * they are associated with v yet) if this allows us to 
      * consider fewer options. We choose not to implement
-     * this as it is very rare. */
-    for (unsigned int i = 0; i < labels.size(); i++) {
+     * this as it is very rare. Furthermore, we know that the 
+     * label (v, emptyset) is always at index 0 in the vector, 
+     * so instead of explicetely testing if J is empty, we simply
+     * start at index 1. */
+    for (unsigned int i = 1; i < labels.size(); i++) {
         Label *J_label = labels[i];
         bitset<BITSET_SIZE> J = J_label->GetBitset();
         
         /* (v,J) should be in P, J should be non-empty, not 
          * contain root and have no terminals in common with I.
          * Since J should not contain root anyways, we do not check this. */
-        if (J_label->IsInP() && J.count() > 0
-                             && (I & J).none()) {
+        if (J_label->IsInP() && (I & J).none()) {
             bitset<BITSET_SIZE> IJ = I | J;
 
             /* If (v, I u J) not yet set, set it and add it to _N and 
@@ -180,9 +182,12 @@ Result Solver::SolveCurrentInstance(int &ret) {
         final_terminal_set.set(i);
 
     /* Add (s, {s}) to _N for each terminal s unequal to 
-     * the root, and set (s, emptyset) for all vertices s. */
-    SetInitialN();
+     * the root, and set (s, emptyset) for all vertices s. 
+     * By calling SetInitialLabels first, we guarantee that
+     * the label (v, emptyset) is always at index 0 in the 
+     * vector containing the labels of v. */
     SetInitialLabels();
+    SetInitialN();
 
     Label *current_label;
 
